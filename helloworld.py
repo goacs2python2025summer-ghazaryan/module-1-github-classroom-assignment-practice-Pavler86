c66c7240cda7e0b749d909e725133f500c28cc1c
Ravindu Karunaratne <rkarunaratne@hamdenhall.org>
2025-06-18 18:21:27 -0400

    Dear Mr. Ghazaryan, Attached is my helloworld.py folder, I made some slight tweaks to it. Thank you!

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+#Send the hello world message
+msg = "Hello world!"
+#print the message
+print(msg)
+#In addition to the existing code, I'm going to make a second message variable so I can use it again
+msgtwo = "Nice to meet you"
+print(msgtwo)
+# Write something about what you did over the last break
+#I decided to have a variable for my response so I could use it again in the future
+msgthree = "Over the last break, I added a new instrument to my repertoire, the Guitar. Now, I play the violin and guitar every morning."
+print("msgthree")
